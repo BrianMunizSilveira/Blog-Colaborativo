d1f28994e35d142bfd32db64067e35661b38e2b7
carlosvfb <158277340+carlosvfb@users.noreply.github.com>
2024-07-08 21:46:06 -0300

    arrumando a index

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,34 +1,37 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./src/**/*.{html,js}'],
-  theme: {
-    extend: {
-      fontFamily: {
-        padrao: ['Oxanium', 'sans-serif'],
-        destaque: ['Figtree', 'sans-serif'],
-        rubik: ['Rubik', 'sans-serif'],
-      },
-      colors: {
-        'azul-destaque': '#1976d2',
-        'azul-hover': '#0d47a1',
-        'ancora': '#00796b',
-        'ancora-hover': '#004d40',
-        'amarelo': '#bdbd3f',
-        'verde': '#008000',
-        'laranja': '#ffa500',
-        'azul': '#0000ff',
-        'vermelho': '#ff0000',
-      },
-      backgroundImage: {
-        'hero-pattern': "url('https://cdn.pixabay.com/photo/2017/04/06/21/38/cube-2209365_1280.jpg')",
-        'cartao-brawhalla': "url('./src/image/cartao-brawhalla.png')",
-        'cartao-capoeira': "url('./src/image/cartao-capoeira.png')",
-        'cartao-cubomagico': "url('./src/image/cartao-cubomagico.png')",
-        'cartao-fotografia': "url('./src/image/cartao-fotografia.png')",
-        'cartao-tatuagem': "url('./src/image/cartao-tatuagem.png')",
-      },
+export const content = ["./src/**/*.{html,js}"];
+export const theme = {
+  extend: {
+    fontFamily: {
+      padrao: ['Oxanium', 'sans-serif'],
+      destaque: ['Figtree', 'sans-serif'],
+      rubik: ['Rubik', 'sans-serif'],
+    },
+    colors: {
+      'azul-destaque': '#1976d2',
+      'azul-hover': '#0d47a1',
+      'ancora': '#00796b',
+      'ancora-hover': '#004d40',
+      'amarelo': '#bdbd3f',
+      'verde': '#008000',
+      'laranja': '#ffa500',
+      'azul': '#0000ff',
+      'vermelho': '#ff0000',
+    },
+    backgroundImage: {
+      'hero-pattern': "url('https://cdn.pixabay.com/photo/2017/04/06/21/38/cube-2209365_1280.jpg')",
+      'cartao-brawlhalla': "url('../image/cartao-brawlhalla.png')",
+      'cartao-capoeira': "url('../image/cartao-capoeira.png')",
+      'cartao-cubomagico': "url('../image/cartao-cubomagico.png')",
+      'cartao-fotografia': "url('../image/cartao-fotografia.png')",
+      'cartao-tatuagem': "url('../image/cartao-tatuagem.png')"
+    },
+    spacing: {
+      'tamanho-cartao': '467px',
+      'largura-cartao': '830px',
+      'padding-negativo': '-90px',
     },
   },
-  plugins: [],
-}
+};
+export const plugins = [];
 
